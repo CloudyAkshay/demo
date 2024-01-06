e9346e2d48e058aed1cd2eb69955f5a4bfb27ed7
Akshay <akshay1668@gmail.com>
2024-01-06 21:48:05 +0530

    first commit

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    username TEXT,
+    email TEXT
+);
+
+INSERT INTO users (username, email) VALUES ('akshay_jadhav', 'akshay1669@.com');
